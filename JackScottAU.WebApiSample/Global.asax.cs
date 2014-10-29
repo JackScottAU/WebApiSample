5a06602efe2fdf9852530afb2967a5595b5c75b0
Jack Scott <jack@jackscott.id.au>
2014-10-29 12:45:15 +1100

    Support for the help area (but not added it yet).

--- JackScottAU.WebApiSample/Global.asax.cs
+++ JackScottAU.WebApiSample/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+//using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace JackScottAU.WebApiSample
@@ -17,6 +18,7 @@ namespace JackScottAU.WebApiSample
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            //AreaRegistration.RegisterAllAreas();
 
             // Web API routes
             config.MapHttpAttributeRoutes();
